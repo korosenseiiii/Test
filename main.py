743e3cd4d1a09650b0b0cbf8bcc43a6181f964f9
stanoelle <118582341+stanoelle@users.noreply.github.com>
2023-06-27 15:07:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ def send_welcome(message):
 
         keyboard.add(button_how_to_use)
 
-        photo = open('Google-Introduces-BARD-AI-Chatbot.jpg', 'rb')
+        photo = open('Google-Introduces-BARD-AI-Chatbot.jpg (1)', 'rb')
 
         bot.send_photo(message.chat.id, photo, caption='''👋 Welcome to our AI-powered bot!
 
